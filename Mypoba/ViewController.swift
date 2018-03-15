79c133657b51fda88568636f3b9a2c9015c7cb9c
행호 강 <lovelittleong@gmail.com>
2018-03-16 00:12:35 +0900

    total source update

--- Mypoba/ViewController.swift
+++ Mypoba/ViewController.swift
@@ -10,16 +10,106 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var initIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    var isStartApp = false;
+    
+    let APPSTORE_PREFIX = "https://itunes.apple.com/in/app/";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        if Reachability.isConnectedToNetwork() == true {
+//            print("conneted")
+//        }else{
+//            print("not connected")
+//        }
+        
+        let str = "https://mypoba.or.kr"
+        
+        if let url = URL(string: str) {
+            let urlRequest = URLRequest(url: url)
+            
+            webView.loadRequest(urlRequest)
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        //print("didReceiveMemoryWarning")
         // Dispose of any resources that can be recreated.
     }
+    
+   
 
 
 }
+
+extension ViewController: UIWebViewDelegate {
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        initIndicator.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        initIndicator.stopAnimating()
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+    }
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        //print("shouldStartLoadWith")
+        if let url = request.url, url.scheme != "http" && url.scheme != "https" {
+            
+            isStartApp = UIApplication.shared.openURL(url);
+            return false;
+        } else {
+            
+            if isStartApp {
+                
+                isStartApp = false;
+                if (request.url?.absoluteString.contains(APPSTORE_PREFIX))! {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            
+            return true;
+        }
+
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        if let url = request.url, url.scheme != "http" && url.scheme != "https" {
+            
+            isStartApp = UIApplication.shared.openURL(url);
+            return false;
+        } else {
+            
+            if isStartApp {
+                
+                isStartApp = false;
+                if (request.url?.absoluteString.contains(APPSTORE_PREFIX))! {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            
+            return true;
+        }
+    }
+
+    
+}
+
+
 
